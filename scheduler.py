0d1156009abf44a16327599c0ef6a9ade211762f
marianamf0 <mmf.marianaf@gmail.com>
2025-07-23 07:52:47 -0300

    Add docstring in scheduler file

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,16 @@ from network import Network
 from settings import Settings
 
 def round_robin_allocation(number_ues:int, number_subcarriers:int):
+    """
+    Allocate subcarriers equally among UEs using a Round-Robin strategy.
+
+    Args:
+        number_ues (int):  Number of UEs in the cell.
+        number_subcarriers (int): Total number of subcarriers to allocate.
+
+    Returns:
+        list: Number of subcarriers allocated to each UE.
+    """
     subcarriers_per_ue = number_subcarriers // number_ues
     subcarriers_allocation = [subcarriers_per_ue] * number_ues
     
@@ -13,6 +23,17 @@ def round_robin_allocation(number_ues:int, number_subcarriers:int):
     return subcarriers_allocation
 
 def max_sinr_allocation(value_sinr:list, number_subcarriers:int):
+    """
+    Allocate subcarriers proportionally to the SINR of each UE.
+    UEs with higher SINR get more subcarriers.
+
+    Args:
+        value_sinr (list): SINR values (linear scale) for each UE
+        number_subcarriers (int): Total number of subcarriers to allocate.
+
+    Returns:
+        list: Number of subcarriers allocated to each UE.
+    """
     subcarriers_allocation = [0]*len(value_sinr)
     sorted_index_ue = np.argsort(-np.array(value_sinr))
     for index_ue in sorted_index_ue:
